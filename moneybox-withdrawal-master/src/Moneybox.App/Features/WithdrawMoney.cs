f6bf60740b61942ec7ac600363d632a0f9ca298c
lbraddick96 <43915750+lbraddick96@users.noreply.github.com>
2019-08-06 20:00:53 +0100

    Add files via upload

--- moneybox-withdrawal-master/src/Moneybox.App/Features/WithdrawMoney.cs
+++ moneybox-withdrawal-master/src/Moneybox.App/Features/WithdrawMoney.cs
@@ -17,7 +17,22 @@ namespace Moneybox.App.Features
 
         public void Execute(Guid fromAccountId, decimal amount)
         {
-            // TODO:
+            var before = this.accountRepository.GetAccountById(fromAccountId);
+
+            var beforeBalance = before.Balance - amount;
+            if (beforeBalance < 0m)
+            {
+                throw new InvalidOperationException("Insufficient funds to make transfer");
+            }
+            if (beforeBalance < 500m)
+            {
+                this.notificationService.NotifyFundsLow(before.User.Email);
+            }
+
+            before.Balance = before.Balance - amount;
+            before.Withdrawn = before.Withdrawn - amount;
+
+            this.accountRepository.Update(before);
         }
     }
 }
